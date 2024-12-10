15ba791c5a2cf60f9cb8bebf6e29ab4cbd8fc473
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-12-10 15:03:15 -0500

    Update indent.tf

--- shorts/str_func/indent/indent.tf
+++ shorts/str_func/indent/indent.tf
@@ -2,6 +2,10 @@ locals {
     val1 = "My name is Srinivas Pradhan.\n I am an SRE."
 }
 
-output "first_name" {
+output "val1" {
   value       = indent(2, local.val1)
+}
+
+output "val2" {
+  value       = indent(4, local.val1)
 }
